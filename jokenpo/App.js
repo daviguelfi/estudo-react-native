ab3e4b494cc053e069606726540a36566dc76530
daviguelfi <daviguelfi@gmail.com>
2019-02-17 01:09:11 -0300

    Logica do jogo finalizada e mudanca de layout

--- jokenpo/App.js
+++ jokenpo/App.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View, Button } from 'react-native';
+import { Platform, StyleSheet, Text, View, Button, Image } from 'react-native';
+import Jokenpo from './img/jokenpo.png';
+import Pedra from './img/pedra.png';
+import Papel from './img/papel.png';
+import Tesoura from './img/tesoura.png';
+
 
 
 export default class App extends Component {
@@ -35,10 +40,10 @@ export default class App extends Component {
         result = 'Empate'
       }
       if (userChoice == 'Papel') {
-        result = 'O usuário venceu'
+        result = 'Você venceu'
       }
       if (userChoice == 'Tesoura') {
-        result = 'O computador venceu'
+        result = 'Você perdeu'
       }
     }
 
@@ -47,10 +52,10 @@ export default class App extends Component {
         result = 'Empate'
       }
       if (userChoice == 'Tesoura') {
-        result = 'O usuário venceu'
+        result = 'Você venceu'
       }
       if (userChoice == 'Pedra') {
-        result = 'O computador venceu'
+        result = 'Você perdeu'
       }
     }
 
@@ -59,10 +64,10 @@ export default class App extends Component {
         result = 'Empate'
       }
       if (userChoice == 'Pedra') {
-        result = 'O usuário venceu'
+        result = 'Você venceu'
       }
       if (userChoice == 'Papel') {
-        result = 'O computador venceu'
+        result = 'Você perdeu'
       }
     }
 
@@ -76,14 +81,70 @@ export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Escolha do computador: {this.state.computerChoice}</Text>
-        <Text>Escolha do usuário: {this.state.userChoice}</Text>
-        <Text>Resultado: {this.state.result}</Text>
-        <Button title="Pedra" onPress={() => { this.jokenpo('Pedra') }} />
-        <Button title="Papel" onPress={() => { this.jokenpo('Papel') }} />
-        <Button title="Tesoura" onPress={() => { this.jokenpo('Tesoura') }} />
+        <Topo></Topo>
+
+        <View style={styles.viewBtn}>
+          <View style={styles.choiceBtn}>
+            <Button title="Pedra" onPress={() => { this.jokenpo('Pedra') }} />
+          </View>
+
+          <View style={styles.choiceBtn}>
+            <Button title="Papel" onPress={() => { this.jokenpo('Papel') }} />
+          </View>
+
+          <View style={styles.choiceBtn}>
+            <Button title="Tesoura" onPress={() => { this.jokenpo('Tesoura') }} />
+          </View>
+        </View>
+
+        <View>
+          <Text style={styles.resultText}> {this.state.result}</Text>
+          <Icone choice={this.state.computerChoice} player='Computador' ></Icone>
+          <Icone choice={this.state.userChoice} player='Você' ></Icone>
+        </View>
       </View>
     );
+  }
+}
+
+class Topo extends Component {
+  render() {
+    return (
+      <View>
+        <Image source={Jokenpo} />
+      </View>
+    );
+  }
+}
+
+class Icone extends Component {
+  render() {
+
+    if (this.props.choice == 'Pedra') {
+      return (
+        <View style={styles.icone}>
+          <Text style={styles.txtPlayer}>{this.props.player}</Text>
+          <Image source={Pedra} />
+        </View>
+      )
+    }
+    else if (this.props.choice == 'Papel') {
+      return (
+        <View style={styles.icone}>
+          <Text style={styles.txtPlayer}>{this.props.player}</Text>
+          <Image source={Papel} />
+        </View>
+      )
+    }
+    else if (this.props.choice == 'Tesoura') {
+      return (
+        <View style={styles.icone}>
+          <Text style={styles.txtPlayer}>{this.props.player}</Text>
+          <Image source={Tesoura} />
+        </View>
+      )
+    }
+    else return false;
   }
 }
 
@@ -94,4 +155,29 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
+  viewBtn: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  choiceBtn: {
+    width: 90,
+  },
+  result: {
+    alignItems: 'center',
+    marginTop: 10
+  },
+  resultText: {
+    fontSize: 22,
+    fontWeight: 'bold',
+    color: 'red',
+    height: 40,
+    marginTop: 5
+  },
+  icone: {
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+  txtPlayer: {
+    fontSize: 18
+  }
 });
